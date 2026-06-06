fde514e1a82ad8808cffcbc75a4d5bf2798aee34
vaneduocuc <van.olivares@duocuc.cl>
2026-06-06 01:05:11 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'miappmovil',
+  webDir: 'www'
+};
+
+export default config;
